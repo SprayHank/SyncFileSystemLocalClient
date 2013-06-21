3b13d860a2945928a7b9e5fe7363860a98b46675
SprayHank <fantasyoui@gmail.com>
2013-06-21 13:56:02 +0800

    类

--- class/SYNC.class.php
+++ class/SYNC.class.php
@@ -1,2 +1,15 @@
 <?php defined('SYNCSYSTEM') || die('No direct script access.');
 
+class SYNC {
+	public static $CONFIG = array();
+	public function init_ignores(){
+		GLOBAL $IGNORES;
+		$IGNORES = '';
+		if(isset(self::$CONFIG['IGNORE_FILE_LIST'])){
+			$IGNORES =  implode('|', self::$CONFIG['IGNORE_FILE_LIST']);
+			$IGNORES = addcslashes($IGNORES, '.');
+			$IGNORES = strtr($IGNORES, array('?' => '.?', '*' => '.*'));
+			$IGNORES = '/^('.$IGNORES.')$/i';
+		}
+	}
+}
