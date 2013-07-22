64a10481e3d9343f48a1d4c4b60238b291717108
SprayHank <fantasyoui@gmail.com>
2013-07-22 22:14:35 +0800

    函数内联

--- init.php
+++ init.php
@@ -258,25 +258,6 @@ HTM;
 }
 
 
-function curlrequest($url, $data, $method = 'post') {
-	$ch = curl_init(); //初始化CURL句柄
-	curl_setopt($ch, CURLOPT_URL, $url); //设置请求的URL
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //设为TRUE把curl_exec()结果转化为字串，而不是直接输出
-	//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method); //设置请求方式
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-HTTP-Method-Override: $method")); //设置HTTP头信息
-	curl_setopt($ch, CURLOPT_POST, 1); //以post方式提交数据
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data); //设置提交的字符串
-	$document = curl_exec($ch); //执行预定义的CURL
-	if(!curl_errno($ch)) {
-		$info = curl_getinfo($ch);
-		echo '<div>Took '.$info['total_time'].' seconds to send a request to '.$info['url'].'</div>';
-	} else {
-		echo 'Curl error: '.curl_error($ch);
-	}
-	curl_close($ch);
-	return $document;
-}
-
 function pulltolocal() {
 	global $SessionSite, $localdir;
 	$reuslt = "";
